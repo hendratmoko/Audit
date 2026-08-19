781e2e263b4f2c2035caabea1380bf9a45d12691
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-08-19 08:54:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  */
 const CONFIG = {
     // Ganti dengan URL Web App hasil deploy Google Apps Script (Deploy > New deployment > Web app)
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzVa6TFAiecbJbTKP4_FrvEVaeRgcSDDh_YiNQOXsoasI5oRCbbrqvEa65Qhj5UUXe_9g/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbyMoWtSpvMij_BGUuWLvBlSCmJrcuYJgEngdtr5AU6MFLY5XG04kg5E9IeFBVM6y5F7/exec",
 
     SCHOOL_NAME: "SMK NEGERI 1 SANDEN",
     APP_NAME: "Sistem Audit Internal SMK",
